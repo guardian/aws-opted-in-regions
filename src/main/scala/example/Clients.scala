e5721c2cf93d0959327eb4db4d6a31860b8b641c
Amina Adewusi <amina.adewusi@guardian.co.uk>
2020-11-09 13:46:07 +0000

    creates a new object to handle AWS client creation per account

---
+++ src/main/scala/example/Clients.scala
@@ -0,0 +1,25 @@
+package example
+
+import com.amazonaws.auth.AWSCredentialsProviderChain
+import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder
+
+
+// TODO: https://github.com/guardian/security-hq/blob/dde136c9c461b641df9aa9efaf956c03ed2b5c90/hq/app/aws/AWS.scala#L22
+
+object Clients {
+  val defaultRegion: Regions = Regions.EU_WEST_1
+  private def credentialsProviderChain(account: String): AWSCredentialsProviderChain = {
+    new AWSCredentialsProviderChain(
+      new ProfileCredentialsProvider(account)
+    )
+  }
+
+  def createClient(account: String) = {
+    AmazonEC2ClientBuilder.standard()
+      .withCredentials(credentialsProviderChain(account))
+      .withRegion(defaultRegion)
+      .build()
+  }
+}
