788b0e5f1211fe2961cc5b1c5b6b0428aab3a278
Amina Adewusi <amina.adewusi@guardian.co.uk>
2020-11-23 14:00:12 +0000

    WIP

--- src/main/scala/example/OptedInRegion.scala
+++ src/main/scala/example/OptedInRegion.scala
@@ -1,13 +1,15 @@
 package example
 
 import com.amazonaws.services.ec2.model.{DescribeRegionsRequest, Filter}
+import example.Model.{Account, Accounts}
+import io.circe.Encoder.AsObject.importedAsObjectEncoder
 import io.circe._
+import io.circe.generic.auto.exportEncoder
+import io.circe.syntax.EncoderOps
 
 import scala.io.Source
 import scala.jdk.CollectionConverters.CollectionHasAsScala
 import scala.util.control.NonFatal
-
-// TODO: https://gist.github.com/katebee/be5635bb5426f569b16cccc5a87386ae
 
 object OptedInRegion {
   def main(args: Array[String]): Unit = {
@@ -16,16 +18,20 @@ object OptedInRegion {
         println("error - missing argument <output file>")
         sys.exit(1)
       }
+      // TODO: add some code that allows you to EITHER pass in a list OR a file name and then processes them appropriately
+      // get the list of comma separated accounts OR get the file name from the parameter
       case accounts :: _ => {
-        // get the file name from the parameter
+        // create a Scala list from the list of comma separated accounts
         val listOfAccounts: List[String] = accounts.split(",").toList
-        // loop through the list of accounts in the file
+        // if the accounts are in a CSV file - loop through the list of accounts in the file
         // val parsedListOfAccounts = getAccounts(listOfAccounts)
         // run the describe regions API on each account
         val optedInRegions = discoverAllOptInRegions(listOfAccounts)
         // convert the result into Json
         // val jsonResponse = resultToJson(optedInRegions)
         // print to the command line
+        //TODO: should we print the successes from the Either as JSON and the failures as printlns to STDOUT?
+        //TODO: OR, should the model change to encapsulate fails and include error messages?
         println(optedInRegions)
       }
     }
@@ -38,8 +44,6 @@ object OptedInRegion {
     result
   }
 
-  // TODO: https://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/ec2/model/Filter.html#Filter--
-  // TODO: https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_Filter.html
   def discoverOptInRegions(account: String): Either[String, List[String]] = {
     val ec2Client = Clients.createClient(account)
     try {
@@ -49,7 +53,7 @@ object OptedInRegion {
         Right(ec2Client.describeRegions(request).getRegions.asScala.toList.map(_.getRegionName))
       } catch {
       case NonFatal(error) => {
-        println(s"${error.getMessage}")
+        println("There's an error! We may not have been able to make the DescribeRegionsRequest to AWS", error)
         Left(error.getMessage)
       }
     } finally {
@@ -61,14 +65,23 @@ object OptedInRegion {
     accounts.map(account => account -> discoverOptInRegions(account)).toMap
   }
 
-  def split(map: Map[String, Either[String, List[String]]]): (Map[String, List[String]], Map[String, List[String]]) = ???
-
-  // new method
-  // split map into two (pattern match)
-  // successes to json
-  // print failures
-
-  def toJson(result: Map[String, List[String]]): Json = {
-    ???
+  // split map into two (pattern match): successes to json and print failures
+  def getDiscoverOptInRegionResults(discoverRegionsResults: Map[String, Either[String, List[String]]]): Unit = {
+    discoverRegionsResults match {
+      case right: Map[accountName, Right[_, _]] => ??? // pass this to the toJson function?
+      case left: Map[_, Left[_, _]] => ??? // println this?
+    }
   }
+
+  // TODO: there must be a better way of doing this!
+  def toJson(successfulResult: Map[String, List[String]]): Json = {
+    Accounts(successfulResult.map(result => Account(result._1, result._2)).toList).asJson
+  }
+
+  //TODO: is it worth declaring some case classes like this to make the type signatures easier to understand above?
+  case class OptInRegions(optInRegions: List[String])
+
+  case class AccountName(name: String)
+
+  case class AccountWithOptedInRegions(account: Map[AccountName, Either[String, OptInRegions]])
 }
